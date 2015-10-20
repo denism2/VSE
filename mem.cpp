fdda6a5b21a545e07b5e01ce51e5d865be1cf2d6
denism2 <denis.blangille@gmail.com>
2015-10-20 16:21:50 +0200

    imple memory with systemc library

---
+++ mem.cpp
@@ -0,0 +1,27 @@
+#include "systemc.h"
+
+SC_MODULE (mem) {
+	sc_in<sc_int<8>> addr;
+	sc_in <bool> wdis;
+	sc_in <bool> wena;
+	sc_inout_rv <16> data;
+	sc_lv<16> ram_data[256];
+	SC_CTOR(mem) {
+		SC_METHOD(read_data);
+		sensitive << addr << wdis << wena;
+		SC_METHOD(write_data);
+		sensitive << addr << wdis << wena << data;
+	}
+	void read_data() {
+		if (!wdis && wena) {
+			data = mem_data[addr.read()];
+		} else {
+			data = "zzzzzzzzzzzzzzzz";
+		}
+	}
+	void write_data() {
+		if (wdis && !wena) {
+			ram_data[addr.read()] = data;
+		}
+	}
+};
