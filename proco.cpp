1fc8d3b51076392f410f6956f528e4e8ce0eb964
denism2 <denis.blangille@gmail.com>
2015-10-28 15:08:09 +0100

    systemc simple processor

--- proco.cpp
+++ proco.cpp
@@ -4,19 +4,21 @@ SC_MODULE (proco) {
 	sc_out<sc_uint<8> > addr;
 	sc_out <bool> write;
 	sc_out_rv <16> data;
-}
-	SC_CTOR(mem) {
+
+	SC_CTOR(proco) {
 		SC_METHOD(send_data);
 		sensitive << addr << write << data;
 	}
 	
-//	void send_data {
-//		if (write) {
-//		
-//		}
-//	}
-//	}
+	void send_data() {
+		if (write) {
+			addr.read();
+			data.read();
+		}
+	}
+};
+
 int sc_main(int argc, char* argv[]) {
 
 
-};
+}
