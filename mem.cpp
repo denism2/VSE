b8c2837c71b93abffd54a662b035ad6d61525dcd
denism2 <denis.blangille@gmail.com>
2015-11-02 14:14:41 +0100

    saving memory

--- mem.cpp
+++ mem.cpp
@@ -1,28 +1,14 @@
-#include "systemc.h"
+#include "mem.h"
 
-SC_MODULE (mem) {
-	sc_in<sc_uint<8> > addr;
-	sc_in <bool> wdis;
-	sc_in <bool> wena;
-	sc_inout_rv <16> data;
-	sc_lv<16> ram_data[256];
-	
-	SC_CTOR(mem) {
-		SC_METHOD(read_data);
-		sensitive << addr << wdis << wena;
-		SC_METHOD(write_data);
-		sensitive << addr << wdis << wena << data;
+void mem::read_data() {
+	if (!wdis && wena) {
+		data = ram_data[addr.read()];
+	} else {
+		data = "zzzzzzzzzzzzzzzz";
 	}
-	void read_data() {
-		if (!wdis && wena) {
-			data = ram_data[addr.read()];
-		} else {
-			data = "zzzzzzzzzzzzzzzz";
-		}
+}
+void mem::write_data() {
+	if (wdis && !wena) {
+		ram_data[addr.read()] = data;
 	}
-	void write_data() {
-		if (wdis && !wena) {
-			ram_data[addr.read()] = data;
-		}
-	}
-};
+}
