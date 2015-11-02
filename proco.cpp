0e935bf541ea8b90f811efd6bf215379ad81d5e5
denism2 <denis.blangille@gmail.com>
2015-11-02 14:11:41 +0100

    saving

--- proco.cpp
+++ proco.cpp
@@ -1,22 +1,17 @@
-#include "systemc.h"
+#include "proco.h"
 
-SC_MODULE (proco) {
-	sc_out<sc_uint<8> > addr;
-	sc_out <bool> write;
-	sc_out_rv <16> data;
+void proco::send_data() {
+	if (writing) {
+		addr.read();
+		data.read();
+	}
+}
 
-	SC_CTOR(proco) {
-		SC_METHOD(send_data);
-		sensitive << addr << write << data;
+void proco::read_data() {
+	if (!writing) {
+		data.write();
 	}
-	
-	void send_data() {
-		if (write) {
-			addr.read();
-			data.read();
-		}
-	}
-};
+}
 
 int sc_main(int argc, char* argv[]) {
 
