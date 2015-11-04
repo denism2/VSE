da48a941895b898946eb5fe7a960ebc77bfb240e
denism2 <denis.blangille@gmail.com>
2015-11-04 11:21:13 +0100

    basic processor module

--- proco.h
+++ proco.h
@@ -3,7 +3,7 @@
 SC_MODULE (proco) {
 	sc_out<sc_uint<8> > addr;
 	sc_out <bool> writing;
-	
+	sc_out <bool> reading;
 	sc_out_rv <16> data;
 	
 	void send_data();
@@ -11,8 +11,8 @@ SC_MODULE (proco) {
 
 	SC_CTOR(proco) {
 		SC_METHOD(send_data);
-		sensitive << addr << writing << data;
+		sensitive << addr << writing << reading << data;
 		SC_METHOD(read_data);
-		sensitive << writing << data;
+		sensitive << writing << reading << data;
 	}
 };
